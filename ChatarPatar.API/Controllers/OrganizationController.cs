1391be199c6332821ed81926175a8e7c929b6d92
Dhruvi Khanpara <khanparadhruvi16@gmail.com>
2026-03-28 21:37:10 +0530

    clean version

---
+++ ChatarPatar.API/Controllers/OrganizationController.cs
@@ -0,0 +1,32 @@
+﻿using ChatarPatar.API.Attributes;
+using ChatarPatar.Application.DTOs.OrganizationInvite;
+using ChatarPatar.Application.ServiceContracts;
+using ChatarPatar.Common.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatarPatar.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrganizationController : ControllerBase
+{
+    private readonly IServiceManager _services;
+
+    public OrganizationController(IServiceManager services)
+    {
+        _services = services;
+    }
+
+    /// <summary>
+    /// Sends an invite to the given email to join the specified organization.
+    /// </summary>
+    [HttpPost("{orgId:guid}/invites")]
+    [Authorize]
+    [RequirePermission(PermissionCheckLogicEnum.All, "org:members:invite")]
+    public async Task<ActionResult<OrganizationInviteResponseDto>> SendInvite([FromRoute] Guid orgId, [FromBody] SendInviteDto dto)
+    {
+        var result = await _services.OrganizationInviteService.SendInviteAsync(orgId, dto);
+        return Ok(result);
+    }
+}
