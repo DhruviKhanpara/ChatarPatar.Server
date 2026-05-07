c135ef9cfe4affcaccda211da99dabf2ec24beae
Dhruvi Khanpara <khanparadhruvi16@gmail.com>
2026-05-07 22:36:42 +0530

    Modified CORS for lovable

--- ChatarPatar.API/Configurations/CorsConfiguration.cs
+++ ChatarPatar.API/Configurations/CorsConfiguration.cs
@@ -11,7 +11,29 @@
                     {
                         var origins = configuration.GetSection("AppSettings:AllowedOrigins").Get<string[]>() ?? Array.Empty<string>();
 
-                        policy.WithOrigins(origins!)
+                        policy.SetIsOriginAllowed(origin =>
+                         {
+                             if (string.IsNullOrWhiteSpace(origin))
+                                 return false;
+
+                             if (origins.Contains(origin,
+                                 StringComparer.OrdinalIgnoreCase))
+                                 return true;
+
+                             // Lovable preview domains
+                             if (origin.EndsWith(".lovable.app",
+                                 StringComparison.OrdinalIgnoreCase))
+                                 return true;
+
+                             // ngrok domains
+                             if (origin.Contains("ngrok-free.app",
+                                 StringComparison.OrdinalIgnoreCase) ||
+                                 origin.Contains("ngrok-free.dev",
+                                 StringComparison.OrdinalIgnoreCase))
+                                 return true;
+
+                             return false;
+                         })
                         .AllowAnyHeader()
                         .AllowAnyMethod()
                         .AllowCredentials();
